de523161fcfd2ea93628ba03e58fdce79b7ee136
Alif <arfshl@protonmail.com>
2025-09-21 16:13:46 +0700

    Update cinnamon.sh

--- cinnamon.sh
+++ cinnamon.sh
@@ -56,7 +56,7 @@ xdg-mime default vlc.desktop video/hevc
 xdg-mime default vlc.desktop video/webm
 xdg-mime default mate-terminal.desktop x-scheme-handler/terminal
 xdg-mime default nemo.desktop inode/directory
-sudo update-alternatives --install /usr/bin/gnome-terminal gnome-terminal /usr/bin/mate-terminal 50
+sudo update-alternatives --install /usr/bin/gnome-terminal gnome-terminal /usr/bin/mate-terminal 101
 cp /usr/share/applications/vlc.desktop $HOME/Desktop/vlc.desktop
 cp /usr/share/applications/firefox.desktop $HOME/Desktop/firefox.desktop
 cp /usr/share/applications/mintinstall.desktop $HOME/Desktop/mintinstall.desktop
