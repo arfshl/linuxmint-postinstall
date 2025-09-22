dde64b299d8061f3ce7a5a15b0fb07d96018db5c
Alif <arfshl@protonmail.com>
2025-09-22 08:29:52 +0700

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -57,7 +57,6 @@ xdg-mime default vlc.desktop video/x-matroska
 xdg-mime default vlc.desktop audio/mpeg
 xdg-mime default vlc.desktop video/hevc
 xdg-mime default vlc.desktop video/webm
-xdg-mime default mate-terminal.desktop x-scheme-handler/terminal
 xdg-mime default nemo.desktop inode/directory
 ln -s /usr/share/applications/vlc.desktop $HOME/Desktop/vlc.desktop
 ln -s /usr/share/applications/firefox.desktop $HOME/Desktop/firefox.desktop
