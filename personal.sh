8bfb1dba5ebd9c7f617c9bf06e459e9911e615fb
arfshl <arfshl@protonmail.com>
2026-01-10 15:18:41 +0700

    Update personal.sh

--- personal.sh
+++ personal.sh
@@ -68,9 +68,6 @@ sudo apt install apt-transport-https -y
 sudo apt update
 sudo apt install code -y # or code-insiders
 
-# install brave
-curl -fsS https://dl.brave.com/install.sh | sh
-
 # install github-desktop
 wget -qO - https://mirror.mwt.me/shiftkey-desktop/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/mwt-desktop.gpg > /dev/null
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/mwt-desktop.gpg] https://mirror.mwt.me/shiftkey-desktop/deb/ any main" > /etc/apt/sources.list.d/mwt-desktop.list'
