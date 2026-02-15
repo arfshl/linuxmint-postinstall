0c824a0a2a890a5563eb85ed83998222a30c4cf4
arfshl <arfshl@protonmail.com>
2026-02-15 18:48:44 +0700

    Update debian-kde.sh

--- personal-use/debian-kde.sh
+++ personal-use/debian-kde.sh
@@ -4,7 +4,7 @@
 # we take example of username is user
 
 # Install required tools
-sudo apt install vlc zram-tools btop htop k3b default-jre wget curl nano git systemd-timesyncd ufw gufw apache2 bind9 simplescreenrecorder -y
+sudo apt install vlc zram-tools btop htop brasero default-jre wget curl nano git systemd-timesyncd ufw gufw apache2 bind9 simplescreenrecorder -y
 
 # install protonvpn
 wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.8_all.deb && sudo dpkg -i ./protonvpn-stable-release_1.0.8_all.deb && sudo rm protonvpn-stable-release_1.0.8_all.deb && sudo apt update && sudo apt install proton-vpn-gnome-desktop
