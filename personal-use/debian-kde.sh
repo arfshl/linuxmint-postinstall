b09cc0d0d150e187c4559b1b6ecf096508e1c06f
arfshl <arfshl@protonmail.com>
2026-02-16 19:47:08 +0700

    Install weathr and update GRUB configuration
    Added rustup and weathr installation, updated GRUB config.

--- personal-use/debian-kde.sh
+++ personal-use/debian-kde.sh
@@ -4,10 +4,59 @@
 # we take example of username is user
 
 # Install required tools
-sudo apt install vlc zram-tools btop htop brasero default-jre wget curl nano git systemd-timesyncd ufw gufw apache2 bind9 simplescreenrecorder -y
+sudo apt install vlc zram-tools btop htop brasero default-jre wget curl nano git systemd-timesyncd ufw gufw apache2 bind9 simplescreenrecorder rustup -y
 
 # install protonvpn
 wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.8_all.deb && sudo dpkg -i ./protonvpn-stable-release_1.0.8_all.deb && sudo rm protonvpn-stable-release_1.0.8_all.deb && sudo apt update && sudo apt install proton-vpn-gnome-desktop
+
+# install weathr rust app
+rustup toolchain install nightly
+cargo install weathr
+sudo ln -s /home/user/.cargo/bin/weathr /usr/bin/weathr
+tee /home/user/.config/weathr/config.toml > /dev/null <<EOF
+# Hide the HUD (Heads Up Display) with weather details
+hide_hud = false
+
+# Run silently without startup messages (errors still shown)
+silent = false
+
+[location]
+# Location coordinates (overridden if auto = true)
+latitude = 
+longitude = 
+
+# Auto-detect location via IP (defaults to true if config missing)
+auto = false
+
+# Hide the location name in the UI
+hide = false
+
+[units]
+# Temperature unit: "celsius" or "fahrenheit"
+temperature = "celsius"
+
+# Wind speed unit: "kmh", "ms", "mph", or "kn"
+wind_speed = "kmh"
+
+# Precipitation unit: "mm" or "inch"
+precipitation = "mm"
+EOF
+
+# generate custom grub config (disable os-prober, block kvm module, and enable verbose boot, amoled black grub wallpaper}
+cp /home/user/1.png /home/user/1.png
+sudo mv /etc/default/grub /etc/default/grub.bak
+sudo tee /etc/default/grub > /dev/null <<EOF
+GRUB_DEFAULT=0
+GRUB_TIMEOUT=-1
+GRUB_DISTRIBUTOR='Debian'
+#GRUB_CMDLINE_LINUX_DEFAULT='quiet splash'
+GRUB_CMDLINE_LINUX_DEFAULT='kvm.enable_virt_at_load=0'
+GRUB_CMDLINE_LINUX=""
+GRUB_BACKGROUND="/home/user/1.png"
+GRUB_DISABLE_OS_PROBER=false
+EOF
+sudo update-grub
+
 
 # disable apt pager
 echo 'Binary::apt::Pager "false";' | sudo tee -a  /etc/apt/apt.conf.d/99nopager
